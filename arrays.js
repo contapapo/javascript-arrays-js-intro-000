a82784d8c21ebba1d27f521542acde45d9c2e029
Julio Martelosso <martelossomartin1506.student@ifosupwavre.be>
2017-11-03 18:29:54 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -10,7 +10,8 @@ function addElementToBeginningOfArray(array, element){
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
-  return array = [element, ...array];
+   array = [element, ...array];
+   return array;
 }
 
 // function addElementToEndOfArray(array,element){
