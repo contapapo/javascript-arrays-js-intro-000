4391082dd8368446fc3c37c014d6119fb35d8aae
Julio Martelosso <martelossomartin1506.student@ifosupwavre.be>
2017-11-03 18:02:55 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,6 +6,6 @@ var chocolateBars = [
 ]
 
 
-function addElementToBeginningOfArray(array, "element"){
-  ["element", ...array];
+function addElementToBeginningOfArray(array, element){
+  [element, ...array];
 }
